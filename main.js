4d604f544684f9ea1f4d9dfbca3404c54eda0d70
Esteban Rinaldi <erinaldi@ssas.gob.ar>
2021-04-26 16:00:42 -0300

    .

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-import http from "http";
+import http from 'http';
 
-const hostname = "localhost";
+const hostname = 'localhost';
 
 /** @type {number} */
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hola Mundo!!");
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hola Mundo!');
 });
 
 server.listen(port, hostname, () => {
-  console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
+  // eslint-disable-next-line no-console
+  console.log(
+    `El servidor se está ejecutando en http://${hostname}:${port}/ esta linea.`,
+  );
 });
