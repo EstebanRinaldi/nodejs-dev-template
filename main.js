9573ae713f4cbe9eb69faa700decfc16b2c3b8df
Esteban Rinaldi <erinaldi@ssas.gob.ar>
2021-04-26 12:36:09 -0300

    .

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-import http from 'http';
+import http from "http";
 
-const hostname = 'localhost';
+const hostname = "localhost";
 
 /** @type {number} */
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hola Mundo!!');
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hola Mundo!!");
 });
 
 server.listen(port, hostname, () => {
